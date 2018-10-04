8e78a4675576e2c8ed47e65d66b2b065d6017d0d
hiaja <banmabanma740@163.com>
2018-10-04 15:06:46 +0800

    insertSort

--- sort/Project1/Project1/源.cpp
+++ sort/Project1/Project1/源.cpp
@@ -1,5 +1,48 @@
 #include<iostream>
+#include <stdlib.h>
 using namespace std;
+
+
+
+void printArray(int a[], int N) {
+	int i;
+	for (i = 0; i < N; ++i) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+void initArray(int a[], int N) {
+	int i;
+	for (i = 0; i < N; ++i) {
+		a[i] = rand();
+	}
+}
+
+
+
+void insertionSort(int a[], int N) {
+	int i, j, temp;
+	for (i = 0; i < N; ++i) {
+		temp = a[i];
+		j = i - 1;
+		while (j >= 0 && a[j] > temp) {
+			a[j + 1] = a[j];
+			--j;
+		}
+		a[j + 1] = temp;
+	}
+
+
+}
+
 int main() {
+	int N = 15;
+	int a[15];
+	initArray(a, N);
+	printArray(a, N);
+	insertionSort(a, N);
+	printArray(a, N);
+	getchar();
 	return 0;
 }
