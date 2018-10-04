24e25142bb4b5daf3a6e25ab34ef20eaf6b24a1d
BanmaBanma <740195185@qq.com>
2018-10-04 15:51:22 +0800

    shell sort

--- sort/Project1/Project1/源.cpp
+++ sort/Project1/Project1/源.cpp
@@ -30,6 +30,27 @@ void insertionSort(int a[], int N) {
 	}
 }
 
+void insertionSort(int a[], int N, int g) {
+	for (int i = g; i < N; ++i) {
+		int temp = a[i];
+		int j = i - g;
+		while (j >= 0 && a[j] > temp) {
+			a[j + g] = a[j];
+			j -= g;
+		}
+		a[j + g] = temp;
+    }
+}
+
+
+void shellSort(int a[], int N) {
+	int g[2] = { 1,4 };
+	for (int i = 1; i >= 0; --i) {
+		insertionSort(a, N, g[i]);
+	}
+}
+
+
 void bubbleSort(int a[], int N) {
 	bool flag = 1;
 	for (int i = 0; flag; ++i) {
@@ -43,13 +64,28 @@ void bubbleSort(int a[], int N) {
 	}
 }
 
+void selectionSort(int a[], int N) {
+	int mini = 0;
+	for (int i = 0; i < N-1; ++i) {
+		mini = i;
+		for (int j = i; j < N; ++j) {
+			if (a[j] < a[mini]) {
+				mini = j;
+			}
+		}
+		swap(a[mini], a[i]);
+	}
+}
+
 int main() {
 	int N = 15;
 	int a[15];
 	initArray(a, N);
 	printArray(a, N);
 //	insertionSort(a, N);
-	bubbleSort(a, N);
+//	bubbleSort(a, N);
+//	selectionSort(a, N);
+	shellSort(a, N);
 	printArray(a, N);
 	getchar();
 	return 0;
